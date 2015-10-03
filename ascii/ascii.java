55727db88afebfe82ae662b88dd1c53a59d48808
cristalle1215 <cristallekwok2gmail.com>
2015-10-02 18:00:28 -0700

    ascii
    art project

---
+++ ascii/ascii.java
@@ -0,0 +1,89 @@
+
+/**
+ * ASCII Art Project
+ * 
+ * Cristalle Kwok
+ * 10/1/15
+ */
+public class ascii
+{   
+public static void r2d2AndC3po(){
+       System.out.println("                Did you hear that?");
+       System.out.println("                     /-\\");
+       System.out.println("                    |oo )");
+       System.out.println("                    _\\=/_");
+       System.out.println("                  /       \\");
+       System.out.println("    ___          // |/.\\| \\\\");
+       System.out.println("   / () \\        \\\\  \\_/  ||");
+       System.out.println(" _|_____|_        \\ |\\ /| ||");
+       System.out.println("| | === | |        # _ _/ #");
+       System.out.println("|_|  O  |_|         | | |");
+       System.out.println(" ||__*__||          | | |");
+       System.out.println("|- \\___/ -|         []|[]");
+       System.out.println("/=\\ /=\\ /=\\         | | |");
+       System.out.println("[_]_[_]_[_]        /_] [_\\");
+    }
+ public static void stormtroopers(){
+    for (int a=1; a<=6; a++){ 
+         System.out.print("  /__\\       ");
+        }
+    System.out.println();
+    for (int b=1; b<=6; b++){
+       System.out.print(" |<><>|      ");
+     }
+       System.out.println();
+    for (int c=1; c<=6; c++){
+       System.out.print(" ( /\\ )      ");
+    }
+      System.out.println();
+    for (int d=1; d<=6; d++){
+       System.out.print("/       \\    ");
+    }
+       System.out.println();
+    for (int e=1; e<=6; e++){   
+       System.out.print("| |/(=== o   ");
+    }
+       System.out.println();
+    for (int f=1; f<=6; f++){
+       System.out.print("|  / \\   |   ");
+    }
+       System.out.println();
+    for (int g=1; g<=6; g++){
+       System.out.print("\\/] [][ \\/   ");
+    }
+       System.out.println();
+    for (int h=1; h<=6; h++){
+       System.out.print(" |\\  / |     ");
+    }
+       System.out.println();
+    for (int i=1; i<=6; i++){
+       System.out.print(" |_||_|      ");
+    }
+       System.out.println();
+    for (int j=1; j<=6; j++){
+       System.out.print(" [ ][ ]      ");
+    }
+       System.out.println();
+    for (int k=1; k<=6;k++){
+       System.out.print(" | || |      ");
+    }
+       System.out.println();
+    for (int l=1; l<=6; l++){
+       System.out.print(" | || |      ");
+    }
+       System.out.println();
+    for (int m=1; m<=6; m++){
+       System.out.print("/__][_]      ");
+    }
+       System.out.println();
+}
+public static void main(String[] args){
+    stormtroopers();
+    System.out.println();
+    stormtroopers();
+    System.out.println();
+    r2d2AndC3po();
+   }
+}
+
+ 
